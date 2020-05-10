ecd8abca66355014c20fa8f48f8a538f5492b226
Basem Mostafa <basem.mostafa98@hotmail.com>
2020-05-10 23:27:31 +0200

    Created basic broadcast functionality with basic styling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
 const config = require('./config/envConfig');
 
-const server = express();
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    socket.broadcast.emit('message', { message, timestamp: (new Date).toUTCString() });
+  });
+}
+);
 
 
 server.listen(config.APP.PORT, () => {
